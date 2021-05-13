3a64ce1d9ad50a6ea813f1677abfe10a0bcdf007
bessam-haj-salem <bessamhajsalem@gmail.com>
2021-05-13 21:49:29 +0100

    first commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './angular-material/material.module';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostsComponent } from './components/posts/posts.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    PostsComponent
+
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
